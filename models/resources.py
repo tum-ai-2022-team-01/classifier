511874093dd8e389c6954070cd0b7d709427b31a
DavidKlueners <davidklueners@gmail.com>
2022-10-01 23:52:33 +0200

    Create resources.py

---
+++ models/resources.py
@@ -0,0 +1,59 @@
+prohibited_dict = {
+    1: """
+        an AI system that deploys
+        subliminal techniques beyond a person’s consciousness with the objective to or the
+        effect of materially distorting a person’s behaviour in a manner that
+        causes or is reasonably likely to cause that person or another person physical or
+        psychological harm
+        """,
+    2: """
+        an AI system that exploits
+        any of the vulnerabilities of a specific group of persons due to their age, disability or social or economic situation, with the objective to or the
+        effect of materially distorting the behaviour of a person pertaining to that
+        group in a manner that causes or is reasonably likely to cause that person or another
+        person physical or psychological harm
+    """,
+    3: """
+        AI systems for the evaluation or classification 
+        of natural persons over a certain period of time based on their social behaviour or
+        known or predicted personal or personality characteristics, with the social score
+        leading to either or both of the following:
+        (i) detrimental or unfavourable treatment of certain natural persons or
+        groups thereof in social contexts which are unrelated to the contexts in which
+        the data was originally generated or collected;
+        (ii) detrimental or unfavourable treatment of certain natural persons or
+        groups thereof that is unjustified or disproportionate to their social behaviour
+        or its gravity;
+    """,
+    4: """
+        the use of ‘real-time’ biometric identification systems in publicly accessible
+        spaces by law enforcement authorities or on their behalf for the purpose of law
+        enforcement, unless and in as far as such use is strictly necessary for one of the
+        following objectives:
+        (i) the targeted search for specific potential victims of crime;
+        (ii) the prevention of a specific and substantial threat to the critical
+        infrastructure, life, health or physical safety of natural persons or the
+        prevention of a terrorist attacks; 
+    """,
+    5: """
+        the, localisation, or identification of a natural person
+        for the purposes of conducting a criminal investigation, prosecution or
+        executing a criminal penalty for offences referred to in Article 2(2) of Council Framework
+        Decision 2002/584/JHA33 and punishable in the Member State concerned by a
+        custodial sentence or a detention order for a maximum period of at least three
+        years, or other specific offences punishable in the Member State
+        concerned by a custodial sentence or a detention order for a maximum
+        period of at least five years, as determined by the law of that Member State.
+    """
+}
+
+high_risk_dict = {
+    1: "Biometric identification systems",
+    2: "Critical infrastructure",
+    3: "Education and vocational training",
+    4: "Employment, workers management and access to self-employment",
+    5: "Access to and enjoyment of essential private services and public services and benefits",
+    6: "Law enforcement",
+    7: "Migration, asylum and border control management",
+    8: "Administration of justice and democratic processes"
+}
