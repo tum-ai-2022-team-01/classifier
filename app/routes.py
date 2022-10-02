99c2303640c3770ad9fe8eee032369a5ec90ff20
Jakob Zmrzlikar <jakob.zmrzlikar@gmail.com>
2022-10-02 09:36:26 +0200

    added main to routes

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,9 @@
 from app import app
 from flask import request
 
-def classifier(text):
-  return {
-    'risk': 'high',
-    'title': 'Prohibited AI use',
-    'description': 'A AI-controlled world domination system (Terminator) is probably a bad idea...'
-  }
+from models import main
 
 @app.route('/')
 def handler():
   text = request.args.get('text', default = '', type = str)
-  return classifier(text)
+  return main(text)
